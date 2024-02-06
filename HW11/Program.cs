e4e19fbfb24bbc398a57b1744471cb0208f72c96
Spaceecs <azaza2046goi@gmail.com>
2024-02-06 09:14:17 +0200

    add Fib()

--- HW11/Program.cs
+++ HW11/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿bool Fib(int num)
+{
+    int a = 0;
+    int b = 1;
+    while (b < num)
+    {
+        int temp = b;
+        b += a;
+        a = temp;
+    }
+    return b == num;
+}
+
+//Task 1
+
+
+Console.Write("Enter Num: ");
+int num;
+Int32.TryParse(Console.ReadLine(),out num);
+Predicate<int> pred = new Predicate<int>(Fib);
+Console.WriteLine($"Result : {pred(num)}"); 
